ddc2a7c43671817ae2d4a2070d67fed4491aee59
Luke Ghenco <lukeghenco@gmail.com>
2017-06-23 08:59:43 -0400

    Rename Surfboards folder

---
+++ src/containers/Surfboards/index.js
@@ -0,0 +1,12 @@
+import React { Compnent } from 'react';
+
+class Surfboards extends Component {
+
+  render() {
+    return (
+      <div>Surfboards</div>
+    )
+  }
+}
+
+export default Surfboards;
